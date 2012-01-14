ed918cdd61a1914282ff6ba47f88ea4fc75be301
Dan <dtrupenn@gmail.com>
2012-01-13 21:40:01 -0500

    I spot you

---
+++ SpotMe/src/com/pennapps/spotme/SpotMeActivity.java
@@ -0,0 +1,13 @@
+package com.pennapps.spotme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SpotMeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
