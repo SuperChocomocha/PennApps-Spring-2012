15dc96d846dc61ff5e2a1b50860db542edb98440
Harmony Li <harmony.mw.li@gmail.com>
2012-01-14 00:04:56 -0500

    spotme files

--- SpotMe/gen/com/pennapps/spotme/R.java
+++ SpotMe/gen/com/pennapps/spotme/R.java
@@ -11,12 +11,20 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int close=0x7f020000;
-        public static final int facebook_icon=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
+        public static final int facebook_icon=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+    }
+    public static final class id {
+        public static final int activateButton=0x7f050001;
+        public static final int button1=0x7f050004;
+        public static final int frameLayout1=0x7f050003;
+        public static final int linearLayout1=0x7f050002;
+        public static final int relativeLayout1=0x7f050000;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int activate=0x7f030000;
+        public static final int deactivate=0x7f030001;
+        public static final int main=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
