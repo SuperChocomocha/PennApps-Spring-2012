fdfe1c88ef0328552f2f932fe81e5052d848b41f
Max Guo <mmguo1001@gmail.com>
2012-01-15 06:08:52 -0500

    updated algorithm and server backend code

--- algo.py
+++ algo.py
@@ -7,6 +7,9 @@ API_SECRET = ""
 ARTIST_A = "Symphony X"
 ARTIST_B = "Death Angel"
 
+TRACK_A = "For Whom the Bell Tolls"
+TRACK_B = "Symphony of Destruction"
+
 W_S = 0.5 # weight on similar artists
 W_T = 0.5 # weight on similar tags
 THRESHOLD = 0.2 # min score to be similar
@@ -14,14 +17,61 @@ THRESHOLD = 0.2 # min score to be similar
 SIMILAR_LIMIT = 20 # number of similar artists to examine
 TAG_LIMIT = 5
 
+LOC_1 = (39.95419, -75.19535) # Sansom East
+LOC_2 = (39.95199, -75.20109) # Harrison
+
+MAX_DISTANCE = 50 * 0.000621371192 # meters * miles/meter
+
 ## Handle for Last.FM API calls
 network = pylast.LastFMNetwork(api_key = API_KEY, api_secret = API_SECRET)
 
-def compute(user, loc):
-    pass
-
 ## Comparison algorithm that returns whether artist1 and artist2 are similar
-def compare(artist1, artist2):
+"""
+def compare(track1, track2, loc1=None, loc2=None):
+
+    ## Grab the top search result for the artists from Last.fm
+    # search takes ~2-3 sec
+    t1 = network.search_for_track("",track1).get_next_page()[0]
+    t2 = network.search_for_track("",track2).get_next_page()[0]
+    a1 = t1.get_artist()
+    a2 = t2.get_artist()
+    #a1 = network.get_artist(artist1)
+    #a2 = network.get_artist(artist2)
+
+    print a1, a2
+
+    ## Grab similar artists
+    similar = a1.get_similar(limit=SIMILAR_LIMIT)
+    similar_artists_1 = map(lambda x: x[0], similar) # grab Artists
+    #similar = a2.get_similar(limit=SIMILAR_LIMIT)
+    #similar_artists_2 = map(lambda x: x[0], similar) # grab Artists
+
+    ## Grab top tags
+    tag_items = a1.get_top_tags(limit=TAG_LIMIT)
+    tags_1 = set(map(lambda x: x[0].get_name(), tag_items))
+    tag_items = a2.get_top_tags(limit=TAG_LIMIT)
+    tags_2 = set(map(lambda x: x[0].get_name(), tag_items))
+
+    #print tags_1, tags_2, tags_1 & tags_2
+
+    #print map(lambda x: x.get_name(), similar_artists)
+
+    s = a2 in similar_artists_1 #or a1 in similar_artists_2
+    t = len(tags_1 & tags_2) / (1.0 * TAG_LIMIT)
+
+    score =  W_S * s + W_T * t
+
+    #print score
+    return score >= THRESHOLD
+"""
+def compare(artist1, artist2, loc1=(0.0,0.0), loc2=(0.0,0.0)):
+
+    ## Check distance using approximation
+    x = 69.1 * (loc1[0] - loc2[0]) # lat
+    y = 53.0 * (loc1[1] - loc2[1]) # lon
+    dist = (x**2 + y**2) ** (1/2.)
+
+    if dist > MAX_DISTANCE: return False
 
     ## Grab the top search result for the artists from Last.fm
     # search takes ~2-3 sec
@@ -55,7 +105,8 @@ def compare(artist1, artist2):
     return score >= THRESHOLD
 
 #t1 = time.time()
-#print ARTIST_A, ARTIST_B, compare(ARTIST_A, ARTIST_B)
+#print ARTIST_A, ARTIST_B, compare(ARTIST_A, ARTIST_B, LOC_1, LOC_2)
+#print compare(TRACK_A, TRACK_B)
 #t2 = time.time()
 #print "took %0.3f s" % (t2-t1)
 
--- server_side.py
+++ server_side.py
@@ -1,9 +1,11 @@
+# connect to MySQL database
 # creates a connection to the app
-# gets json data from the connection
+# gets data from the connection
 # stores data into database
 # queries database and runs comparison algorithm
 # returns result of comparison
 
+import ast
 import socket
 import fcntl
 import struct
@@ -27,6 +29,7 @@ def get_ip_address(ifname):
 # connect to database
 sqldb = MySQLdb.connect(host = "", user = "", passwd = "", db = "")
 writer = sqldb.cursor()
+query = sqldb.cursor()
 
 # create socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -34,50 +37,88 @@ s.bind((get_ip_address('wlan0'), __PORT))
 
 s.listen(__MAX_CONNECT)
 
-# start accepting connections and grabbing data
+# accept connections, grab data, analyze data
 while 1:
-    # (client, address) = s.accept()
-    # data = json.loads(client.recv(__RECV_BUF))
-
-    query = sqldb.cursor()
-    query.execute("""
-                  SELECT artist FROM test_artist
-                  WHERE user_id != %s
-                  """, (5)) # generate id
-    result = query.fetchall()
-
-    for item in result:
-        print item
-        print compare("Metallica", item[0]) # first argument is user id artist
+    # connect to a client
+    (client, address) = s.accept()
     
-    query.execute("""
-                  SELECT user_id FROM test_artist
-                  WHERE user_id = %s
-                  """, (5)) # make general
-    result = query.fetchall()
-    
-    if result:
-        writer.execute("""
-                       UPDATE test_artist
-                       SET song = %s,
-                           artist = %s,
-                           album = %s
-                       WHERE
-                           user_id = %s
-                       """, ("Oroborus", "Gojira", "The Art of Dying", 1)) # generalize input
+    # grabs data, splits on newline, strips "json: " and whitespace, and converts into a dict
+    data = ast.literal_eval(str.strip(str.split(client.recv(__RECV_BUF), '\n')[1][6:]))
+
+    # check if it's song info or location info
+    if 'latitude' in data:
+        query.execute("""
+                      SELECT fid FROM location_info
+                      WHERE fid = %s
+                      """, (data['FID']))
+        result = query.fetchall()
+
+        if result: # user exists
+            writer.execute("""
+                           UPDATE location_info
+                           SET latitude = %s,
+                               longitude = %s
+                           WHERE
+                               fid = %s
+                          """, (data['lat'], data['long'], data['FID']))
+        else: # user doesn't exist
+            writer.execute("""
+                           INSERT INTO location_info (fid, latitude, longitude)
+                           VALUES
+                           (%s, %s, %s)
+                          """, (data['FID'] , data['lat'], data['long']))
     else:
-        writer.execute("""
-                       INSERT INTO test_artist (user_id, song, artist, album)
-                       VALUES
-                       (%s, %s, %s, %s)
-                       """, (5 , "Truce", "Death Angel", "Relentless Retribution")) # generalize input
+        query.execute("""
+                      SELECT fid FROM song_info
+                      WHERE fid = %s
+                      """, (data['FID']))
+        result = query.fetchall()
 
-    # client.send("bitch")
-    # query.close()
-    # client.close()
-    break
+        if result:
+            writer.execute("""
+                           UPDATE song_info
+                           SET album = %s,
+                               title = %s,
+                               name = %s,
+                               artist = %s
+                           WHERE
+                               fid = %s
+                          """, (data['Album'], data['Title'], data['Name'], data['Artist'], data['FID']))
+        else:
+            writer.execute("""
+                           INSERT INTO song_info (fid, album, title, name, artist)
+                           VALUES
+                           (%s, %s, %s, %s, %s)
+                          """, (data['FID'] , data['Album'], data['Title'], data['Name'], data['Artist']))
+    
+    # grab all users currently in database that is not the connected user
+    query.execute("""
+                  SELECT artist, latitude, longitude, album, title, name FROM joined_info
+                  WHERE fid != %s
+                  """, (data['FID']))
+    result = query.fetchall()
+    
+    # grab current user's data
+    query.execute("""
+                  SELECT artist, latitude, longitude FROM joined_info
+                  WHERE fid = %s
+                  """, (data['FID']))
+    current_user= query.fetchall() # check if always one output
+
+    # run compare on all pairs of (current user artist, other user artist, current location, other location)
+    r = []
+    if result and current_user:
+        for item in result:
+            b = compare(current_user[0], item[0], (current_user[1], current_user[2]), (item[1], item[2]))
+            if b:
+                r.append({"Artist": item[0], "Latitude": item[1], "Longitude": item[2], "Album": item[3], "Title": item[4], "Name": item[5]})
+    
+    # send results of compare back to current user and close connection
+    client.send(json.dumps({"k": r}))
+    client.close()
 
 # close socket and db
+query.close()
 writer.close()
 s.close()
 
